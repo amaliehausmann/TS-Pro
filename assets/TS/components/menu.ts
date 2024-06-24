d1e5117e69a65211bd9cae29d13ea314d4664528
amaliehausmann <amaliehausmann@gmail.com>
2024-06-24 13:08:57 +0200

    first commit

---
+++ assets/TS/components/menu.ts
@@ -0,0 +1,8 @@
+export function menuToggle (openArrow: string, closeArrow: string, className: string, menuContent: string){
+    let openMenuArrow = document.getElementById(openArrow);
+    let closeMenuArrow = document.getElementById(closeArrow);
+    let navContent = document.getElementById(menuContent);
+    
+    
+
+}
